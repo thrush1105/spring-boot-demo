001f133340f5affbddc3babe1c1bc93674c90130
thrush1105 <thrush1105@gmail.com>
2023-11-25 15:02:59 +0900

    JWT検証APIを作成

--- src/main/java/com/example/demo/controller/JwtController.java
+++ src/main/java/com/example/demo/controller/JwtController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import java.text.ParseException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -7,16 +8,20 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.exception.AppException;
 import com.example.demo.service.JwkService;
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.JWSHeader;
 import com.nimbusds.jose.JWSSigner;
 import com.nimbusds.jose.crypto.RSASSASigner;
+import com.nimbusds.jose.crypto.RSASSAVerifier;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
@@ -52,6 +57,7 @@ public class JwtController {
                 .expirationTime(new Date(now.getTime()
                         + Long.valueOf(environment.getProperty("jwt.expiration", "300")) * 1000))
                 .jwtID(UUID.randomUUID().toString())
+                .claim("foo", "bar")
                 .build();
 
         JWSSigner signer = new RSASSASigner(rsaKey);
@@ -59,13 +65,45 @@ public class JwtController {
         SignedJWT signedJWT = new SignedJWT(header, claimsSet);
         signedJWT.sign(signer);
 
-        Map<String, Object> encoded = new HashMap<>();
-        encoded.put("header", header.toJSONObject());
-        encoded.put("payload", claimsSet.toJSONObject());
+        Map<String, Object> decoded = new HashMap<>();
+        decoded.put("header", header.toJSONObject());
+        decoded.put("payload", claimsSet.toJSONObject());
 
         Map<String, Object> response = new HashMap<>();
         response.put("encoded", signedJWT.serialize());
-        response.put("decoded", encoded);
+        response.put("decoded", decoded);
+        response.put("jwk", rsaPublicKey.toJSONObject());
+
+        return response;
+    }
+
+    @GetMapping("verify")
+    public Map<String, Object> verifyJWT(@RequestParam(name = "token") String token) {
+        RSAKey rsaPublicKey = jwkService.getRsaPublicKey();
+
+        SignedJWT signedJWT;
+        try {
+            signedJWT = SignedJWT.parse(token);
+        } catch (ParseException e) {
+            throw new AppException(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+
+        boolean verified;
+        try {
+            RSASSAVerifier verifier = new RSASSAVerifier(rsaPublicKey);
+            verified = signedJWT.verify(verifier);
+        } catch (JOSEException e) {
+            throw new AppException(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+
+        Map<String, Object> decoded = new HashMap<>();
+        decoded.put("header", signedJWT.getHeader().toJSONObject());
+        decoded.put("payload", signedJWT.getPayload().toJSONObject());
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("encoded", token);
+        response.put("decoded", decoded);
+        response.put("verified", verified);
         response.put("jwk", rsaPublicKey.toJSONObject());
 
         return response;
