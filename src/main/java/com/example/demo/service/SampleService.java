ab4a7fbf87d9622dfb0508e853d1944587b143fc
thrush1105 <thrush1105@gmail.com>
2023-12-03 13:19:11 +0900

    CsvMapperとObjectMapperの衝突によるバグを解消

--- src/main/java/com/example/demo/service/SampleService.java
+++ src/main/java/com/example/demo/service/SampleService.java
@@ -2,19 +2,25 @@ package com.example.demo.service;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
 import java.util.Map;
 
 import org.apache.ibatis.cursor.Cursor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.demo.model.Sample;
 import com.example.demo.repository.SampleRepository;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.SequenceWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 
@@ -29,10 +35,18 @@ public class SampleService {
 
     private final CsvSchema csvSchema;
 
-    public SampleService(SampleRepository sampleRepository, RestTemplate restTemplate, CsvMapper csvMapper) {
+    public SampleService(SampleRepository sampleRepository, RestTemplate restTemplate,
+            @Value("${spring.jackson.date-format:}") String dateFormat) {
         this.sampleRepository = sampleRepository;
         this.restTemplate = restTemplate;
-        this.csvMapper = csvMapper;
+        this.csvMapper = CsvMapper.builder()
+                .configure(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY, false) // カラムの自動ソートを無効にする。
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false) // 日付をタイムスタンプとして出力しない。
+                .propertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE) // カラム名はスネークケースを使用する。
+                .build();
+        if (StringUtils.hasLength(dateFormat)) {
+            this.csvMapper.setDateFormat(new SimpleDateFormat(dateFormat));
+        }
         csvSchema = csvMapper.schemaFor(Sample.class)
                 .rebuild()
                 .setUseHeader(true) // ヘッダ行を出力する。
