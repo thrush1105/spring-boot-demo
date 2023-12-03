adf043f83bd470fefc55a3daf148b0702f212164
thrush1105 <thrush1105@gmail.com>
2023-12-03 14:44:27 +0900

    メール送信の非同期処理を実現

--- src/main/java/com/example/demo/config/AppConfig.java
+++ src/main/java/com/example/demo/config/AppConfig.java
@@ -1,15 +1,28 @@
 package com.example.demo.config;
 
+import java.util.concurrent.Executor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@EnableAsync
 public class AppConfig {
 
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.initialize();
+        return executor;
     }
 
 }
--- src/main/java/com/example/demo/service/MailService.java
+++ src/main/java/com/example/demo/service/MailService.java
@@ -1,11 +1,17 @@
 package com.example.demo.service;
 
+import java.util.concurrent.CompletableFuture;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class MailService {
 
     private final MailSender mailSender;
@@ -20,13 +26,19 @@ public class MailService {
         this.mailSender = mailSender;
     }
 
-    public void sendTestMail() {
+    @Async
+    public CompletableFuture<Void> sendTestMail() {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom(from);
         message.setTo(to);
         message.setSubject("テストメール");
         message.setText("Spring Bootによるメール送信のテストです。");
+
+        log.debug("Started sending email: " + message.toString());
         mailSender.send(message);
+        log.debug("Finished sending email: " + message.toString());
+
+        return CompletableFuture.completedFuture(null);
     }
 
 }
